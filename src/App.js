25bd90723d7a65efaf647ca667bacd21b41b73c6
Jung Su <wjdwjdtn92@naver.com>
2023-01-16 00:22:38 +0900

    🔨 header 구현

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import PostListPage from './pags/PostListPage.js';
 import PostUploadPage from './pags/PostUploadPage.js';
 
 export default function App({ $target }) {
-  new Header({ $target });
+  const header = new Header({ $target, initialState: false });
   const $main = document.createElement('main');
   $main.className = 'main';
   $target.appendChild($main);
@@ -19,14 +19,18 @@ export default function App({ $target }) {
     }
 
     if (pathname.includes('/post/')) {
+      header.setState(true);
       new PostDetailPage({ $target: $main });
       return;
     }
 
     if (pathname.includes('/upload')) {
+      header.setState(true);
       new PostUploadPage({ $target: $main });
       return;
     }
+
+    $target.innerHTML = 'page not nound';
   };
 
   this.route();
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,10 +1,35 @@
-export default function Header({ $target }) {
+import Icon from '../../assets/images/icon_arrow_back.svg';
+
+export default function Header({ $target, initialState }) {
+  this.state = initialState;
   const $header = document.createElement('header');
   $header.className = 'header';
   $target.appendChild($header);
 
+  this.setState = (newState) => {
+    if (newState === this.state) {
+      return;
+    }
+
+    this.state = newState;
+    this.render();
+  };
+
   this.render = () => {
-    return;
+    const bakcButton = this.state
+      ? `<button aria-label="뒤로가기 버튼">
+          <img src=${Icon} alt="뒤로가기 이미지"/>
+        </button>`
+      : '';
+
+    $header.innerHTML = `
+    <nav class="navigation">
+      ${bakcButton}
+      <h1 class="title">
+        HPNY 2023
+      </h1>
+    </nav>
+    `;
   };
 
   this.render();
