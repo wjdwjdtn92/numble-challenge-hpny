38757d681308bff1ca47ba4759eeabeacc4c3fa7
Jung Su <wjdwjdtn92@naver.com>
2023-01-17 15:36:21 +0900

    ♻️ button 컴포넌트 적용

--- src/components/PostList/PostNewButton.js
+++ src/components/PostList/PostNewButton.js
@@ -1,17 +1,22 @@
 import newBtnImg from '../../assets/images/icon_new_btn.svg';
+import Button from '../../UI/Button';
 
-export default function PostNewButton({ $target }) {
-  const $button = document.createElement('button');
-  $button.className = 'button';
-  $button.ariaLabel = '새 글 작성하기 버튼';
-  $target.appendChild($button);
-  console.log('asdasd');
-
+export default function PostNewButton({ $target, onClick }) {
   this.render = () => {
-    $button.innerHTML = `
+    const $children = `
       <img src=${newBtnImg} alt="new post icon"/>
       새 글 작성하기
     `;
+
+    new Button({
+      $target,
+      $children,
+      attributes: {
+        className: 'button',
+        ariaLabel: '새 글 작성하기 버튼',
+      },
+      onClick: onClick,
+    });
   };
 
   this.render();
