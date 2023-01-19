76b34d61114143d9617bc74487e82d9cf133e7a9
Jung Su <wjdwjdtn92@naver.com>
2023-01-19 12:39:05 +0900

    ♻️ 헤더 컴포넌트의 불필요하게 세분화되어 있어 병합

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,9 +1,11 @@
-import HeaderNav from './HeaderNav';
+import Icon from '../../assets/images/icon_arrow_back.svg';
+import { routeChage } from '../../router';
+import classes from './Header.module.css';
 
 export default function Header({ $target, initialState }) {
   this.state = initialState;
   const $header = document.createElement('header');
-  $header.className = 'header';
+  $header.className = classes.header;
   $target.appendChild($header);
 
   this.setState = (newState) => {
@@ -17,16 +19,46 @@ export default function Header({ $target, initialState }) {
 
   this.render = () => {
     $header.innerHTML = '';
-    new HeaderNav({ $target: $header, props: this.state });
+    $header.insertAdjacentHTML(
+      'beforeend',
+      `
+      <nav class=${classes['header__nav']}>
+        ${
+          this.state
+            ? `<button class=${classes['header__nav-back-button']}>
+              <img src=${Icon} alt="뒤로가기 이미지"/>
+              </button>`
+            : ''
+        }
+        <h1 class=${classes['header__nav-title']}>
+          HPNY 2023
+        </h1>
+      </nav>
+    `,
+    );
   };
 
   this.render();
 
   $header.addEventListener('click', (event) => {
-    const $button = event.target.closest('button');
+    const $nav = event.target.closest('nav');
+    console.log($nav);
 
-    if ($button) {
+    if (!$nav) {
+      return;
+    }
+
+    if (event.target.tagName === 'BUTTON') {
       history.back();
+      return;
+    }
+
+    if (event.target.tagName === 'H1') {
+      const { pathname } = location;
+      if (pathname !== '/') {
+        routeChage('/');
+      }
+      return;
     }
   });
 }
