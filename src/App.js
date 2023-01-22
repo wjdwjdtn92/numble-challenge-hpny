9aac482e1dfae6021034850ac6591c6d62762b5b
Jung Su <wjdwjdtn92@naver.com>
2023-01-22 17:49:19 +0900

    🩹 dom과 일반 변수 구분을 위해 $표시 제거

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Header from './components/layout/Header.js';
-import PostDetailPage from './pags/PostDetailPage.js';
+import Main from './components/layout/Main.js';
+import Section from './components/layout/section.js';
+
 import PostEditPage from './pags/postEditPage.js';
 import PostListPage from './pags/PostListPage.js';
+import PostDetailPage from './pags/PostDetailPage.js';
 import PostUploadPage from './pags/PostUploadPage.js';
+
 import { router } from './router.js';
-import Main from './components/layout/Main.js';
 import classes from './App.module.css';
-import Section from './components/layout/section.js';
 
 export default function App({ $target }) {
   this.$element = document.createElement('div');
@@ -14,24 +16,23 @@ export default function App({ $target }) {
   $target.appendChild(this.$element);
 
   const header = new Header({ $target: this.$element, initialState: false });
-  const $main = new Main({ $target: this.$element });
-  const $section = new Section({ $target: $main.$element });
-  console.log($section);
+  const main = new Main({ $target: this.$element });
+  const section = new Section({ $target: main.$element });
 
   this.route = () => {
     const { pathname } = location;
-    $section.$element.innerHTML = '';
+    section.$element.innerHTML = '';
 
     if (pathname === '/') {
       header.setState(false);
-      new PostListPage({ $target: $section.$element });
+      new PostListPage({ $target: section.$element });
       return;
     }
 
     if (pathname.includes('/post/')) {
       header.setState(true);
       new PostDetailPage({
-        $target: $section.$element,
+        $target: section.$element,
         postId: pathname.split('/post/')[1],
       });
       return;
@@ -40,7 +41,7 @@ export default function App({ $target }) {
     if (pathname.includes('/edit/')) {
       header.setState(true);
       new PostEditPage({
-        $target: $section.$element,
+        $target: section.$element,
         postId: pathname.split('/edit/')[1],
       });
       return;
@@ -48,11 +49,11 @@ export default function App({ $target }) {
 
     if (pathname.includes('/upload')) {
       header.setState(true);
-      new PostUploadPage({ $target: $section.$element });
+      new PostUploadPage({ $target: section.$element });
       return;
     }
 
-    $section.innerHTML = 'page not nound';
+    section.innerHTML = 'page not nound';
   };
 
   router(this.route);
